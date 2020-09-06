621834e8ff7c9721acb876fb7a5013dca1a7438f
minkukjo <strike0115@naver.com>
2020-09-06 23:27:20 +0900

    JSX + Props 전달

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from '../logo.svg';
-import './App.css';
+
+interface FoodFavPros {
+  fav : String
+}
+
+function Food(props:FoodFavPros) {
+  return <div>
+    hi {props.fav}
+  </div>
+}
+
+// const Food: React.SFC<FoodFavPros> = ( { fav }) => (
+//   <div>
+//     내 이름은 {fav}
+//   </div>
+// )
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      Hi Guys
+      <Food fav="kimchi"/>
     </div>
   );
 }
