85f3682d4ca2aac4cca14e7d178e24a741fe438d
minkukjo <strike0115@naver.com>
2020-09-17 19:44:31 +0900

    replace 추가

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -12,7 +12,9 @@ const Movie: React.FC<Movie> = ({ title, subtitle, image, userRating }) => {
     <div className="movie">
       <img src={image}></img>
       <div className="movie__data">
-        <h3 className="movie__title">{title}</h3>
+        <h3 className="movie__title">
+          {title.replace('<b>', '').replace('</b>', '')}
+        </h3>
         <h5 className="movie__subtitle">{subtitle}</h5>
         <p className="moive__userRating">{userRating}</p>
       </div>
